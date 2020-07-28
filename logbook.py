e4a77d258ca18ec8bb4b5f8abe5e1aac741bdc12
hamiltona <hamiltona@stu.otc.school.nz>
2020-07-28 13:26:36 +1200

    Added Variables section Added commandCode variable to variables section Added List recentEntries Added userInput function Changed read method on disclaimerFile

--- logbook.py
+++ logbook.py
@@ -3,11 +3,26 @@ import time
 import os
 from datetime import datetime
 
+#Variables
+try:
+    commandFile = open("Program Data","r")
+    commandCode = commandFile.readline()
+    commandFile.close()
+except FileNotFoundError:
+    print('File"Program" Data not found')
+
+#Recent Entries List
+recentEntries = []
+
 #Disclaimer Function
 def disclaimer():
     try:
-        disclaimerFile = open("Disclaimer")
+        disclaimerFile = open("Disclaimer","r")
         print(disclaimerFile.read())
         disclaimerFile.close()
     except FileNotFoundError:
         print("Disclaimer file not found")
+
+#User Data Collection
+def userInput():
+    firstName = str(input("Name?"))
