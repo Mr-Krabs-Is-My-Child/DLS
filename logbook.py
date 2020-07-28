8e7a02b1808a729bea7ce6a5d92652f4bbd8c093
hamiltona <hamiltona@stu.otc.school.nz>
2020-07-28 13:03:22 +1200

    Added Imports for Time, DateTime and OS Added Disclaimer Function Added FileNotFoundError argument to Disclaimer function

--- logbook.py
+++ logbook.py
@@ -1,1 +1,13 @@
+#Imports for program operation
+import time
+import os
+from datetime import datetime
 
+#Disclaimer Function
+def disclaimer():
+    try:
+        disclaimerFile = open("Disclaimer")
+        print(disclaimerFile.read())
+        disclaimerFile.close()
+    except FileNotFoundError:
+        print("Disclaimer file not found")
